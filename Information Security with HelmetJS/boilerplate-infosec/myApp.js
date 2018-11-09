e4be18ed3f98048e263a35f9d1fd2fbdddd38958
Andrey Ruíz <davkorss@gmail.com>
2018-11-09 16:58:15 -0500

    Mitigate the Risk of Cross Site Scripting (XSS) Attacks with helmet.xssFilter()

--- Information Security with HelmetJS/boilerplate-infosec/myApp.js
+++ Information Security with HelmetJS/boilerplate-infosec/myApp.js
@@ -69,7 +69,7 @@ app.use(helmet.frameguard({ action: 'deny' }))
 
 // Use `helmet.xssFilter()`
 
-
+app.use(helmet.xssFilter())
 
 /** 5) Avoid inferring the response MIME type - `helmet.noSniff()` */
 
