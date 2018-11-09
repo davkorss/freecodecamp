cac5558e988dd351fda134d5f5481f8d49f5a690
Andrey Ruíz <davkorss@gmail.com>
2018-11-09 17:02:22 -0500

    Set a Content Security Policy with helmet.contentSecurityPolicy()

--- Information Security with HelmetJS/boilerplate-infosec/myApp.js
+++ Information Security with HelmetJS/boilerplate-infosec/myApp.js
@@ -175,7 +175,7 @@ app.use(helmet.noCache())
 // in the `"'self'"` keyword, the single quotes are part of the keyword itself, 
 // so it needs to be enclosed in **double quotes** to be working.
 
-
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com'] } }))
 
 /** TIP: */ 
 
