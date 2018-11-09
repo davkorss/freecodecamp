32cba8b20f083b1bae721231d58e595cf1b6ae8d
Andrey Ruíz <davkorss@gmail.com>
2018-11-09 16:53:34 -0500

    Hide Potentially Dangerous Information Using helmet.hidePoweredBy()

--- boilerplate-infosec/myApp.js
+++ boilerplate-infosec/myApp.js
@@ -31,7 +31,7 @@ const helmet = require('helmet')
 
 // Use `helmet.hidePoweredBy()``
 
-
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
 
